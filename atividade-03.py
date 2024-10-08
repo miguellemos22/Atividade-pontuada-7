046497d7ddd486636ae9bc5c50fbe8f514ec69ff
Miguel <143547917+miguellemos22@users.noreply.github.com>
2024-10-08 18:01:38 +0000

    atv

--- atividade-03.py
+++ atividade-03.py
@@ -10,11 +10,18 @@ nomes = []
 idades = []
 alturas = []
 pesos = []
+sobrenomes = []
 
 # Solicitando os dados dos usuários em um loop
 while True:
     logoSenai()
     nome = input("Digite o nome do usuário (ou digite 'sair' para encerrar): ")
+    if nome.lower() == 'sair':
+        break
+
+    sobrenome = input("digite seu sobrenome( ou digite 'sair' para encerrar): ")
+    if nome.lower() == 'sair':
+        break
     
     # Verificando se o usuário quer sair
     if nome.lower() == 'sair':
@@ -29,14 +36,41 @@ while True:
     idades.append(idade)
     alturas.append(altura)
     pesos.append(peso)
+    sobrenomes.append(sobrenome)
+
+    def calcular_imc(peso, altura):
+        """calcula o índice de massa corporal(IMC)."""
+        return peso / (altura ** 2)
+    
+    def classificar_IMC(imc):
+        """classifica o IMC em categorias."""
+        if imc < 18.5:
+            return "abaixo do peso"
+        elif 18.5 <= imc < 30:
+            return "peso normal"
+        elif 25 <= imc < 30:
+            return "sobrepeso"
+        else:
+            return "Obesidade"
+    print("/nResultados:")
 
 # Exibindo os dados armazenados
 logoSenai()
 print("\nDados dos usuários:")
 for i in range(len(nomes)):
     print(f"Usuário {i+1}:")
-    print("Nome:", nomes[i])
+    print(f"Nome:", nomes[i], sobrenomes[i])
     print("Idade:", idades[i])
     print("Altura:", alturas[i], "metros")
     print("Peso:", pesos[i], "quilogramas")
-    print()
+
+#calculando o imc    
+imc = calcular_imc(pesos[i], alturas[i])
+
+#classificando o imc
+classificacao = classificar_IMC(imc)
+
+#exibindo resultados
+print(f"IMC:  {imc:.2f}")
+print(f"classificação: {classificacao}\n")
+
